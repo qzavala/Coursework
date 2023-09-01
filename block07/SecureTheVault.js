b01b48c37f1c66e5ef662cd59c938ad07bff37d0
Quiryat Zavala <q.zavala@Quiryats-MBP.attlocal.net>
2023-09-01 14:16:37 -0700

    First commit

---
+++ block07/SecureTheVault.js
@@ -0,0 +1,18 @@
+// This will be the message pop up, people will see
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination")
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination"
+// This is the combination code
+let NumberOne = 20-10;
+let NumberTwo = 20+20;
+let NumberThree = 78/2;
+// This will the combined secret code
+console.log(NumberOne,NumberTwo,NumberThree)
+
+function displayAlert(){
+    alert (`${message} \n ${NumberOne}  ${NumberTwo}  ${NumberThree} `)
+}
+
+displayAlert()
+
+console.log("hello world!")
+
