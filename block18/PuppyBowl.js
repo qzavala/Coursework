cb5a962b2b553fde1a3dfeeffcc685e52eb48acf
quiryat zavala <qzavala2014@gmail.com>
2023-10-23 14:30:44 -0700

    block18

---
+++ block18/PuppyBowl.js
@@ -0,0 +1,16 @@
+                      //Puppy Bowl Workshop
+
+// When user loads in all players are shown
+// Expect players to have two buttons
+
+// When player clicks "see details" button they are shown players name, breed and assigned team
+// Expect a remove button 
+
+// When player clicks remove button that player if taken off roster
+// Expect an add player option on the page
+
+// When player is added they are required to fill out player name and player's breed
+// expect a submit button 
+
+// When player is successfully made the "new player" is added to roster
+// When a player is removed they disapear from roster without a refresh
