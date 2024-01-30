b8df7d9895b046ed58fe2313865acc24f38d298a
quiryat zavala <qzavala2014@gmail.com>
2024-01-30 15:34:50 -0800

    added imports into contactlist

---
+++ block26/contact-list/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+import './App.css'
+import ContactList from './components/ContactList'
+
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
+
+// export default App
